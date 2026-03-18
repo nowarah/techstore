5ea69c7fb5a4f68e1fa65275fedebea6dc91a4f8
nuwarah <nr.khalfallah@gmail.com>
2026-03-18 11:04:19 +0100

    CI/CD

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'trailing_comma_in_multiline' => true,
+        'concat_space' => ['spacing' => 'one'],
+    ])
+    ->setFinder($finder)
+    ->setCacheFile('.php-cs-fixer.cache')
+;
